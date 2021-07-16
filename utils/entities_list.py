8c00062d8ac118cc02d0ac8e96b1f7700fbd100d
Kaoru TANAKA <kaoru.tanaka@acm.org>
2021-07-16 14:48:23 +0900

    add entitiy labels

--- utils/entities_list.py
+++ utils/entities_list.py
@@ -1,3 +1,26 @@
 # -*- coding: utf-8 -*-
 
-Entities_list = ['title', 'inv_id', 'cust_name', 'cust_addr', 'vend_name', 'vend_addr', 'inv_date', 'acc_bank', 'acc_branch', 'acc_type', 'acc_num', 'acc_name', 'due_date', 'total', 'subtotal', 'tax', 'billing']
+Entities_list = [
+    'title',
+    'inv_id',
+    'cust_name',
+    'cust_addr',
+    'cust_zip',
+    'vend_name',
+    'vend_addr',
+    'vend_tel',
+    'vend_fax',
+    'vend_mail',
+    'vend_zip'
+    'inv_date',
+    'acc_bank',
+    'acc_branch',
+    'acc_type',
+    'acc_num',
+    'acc_name',
+    'due_date',
+    'total',
+    'subtotal',
+    'tax',
+    'billing',
+]
