6e6f2d81460a53c89234b08c9088c7b618f87e51
Kaoru TANAKA <kaoru.tanaka@acm.org>
2022-02-07 11:22:05 +0900

    update parameters
    MAX_BOXES_NUM, MAX_TRANSCRIPT_LEN, and Entities_list

--- utils/entities_list.py
+++ utils/entities_list.py
@@ -1,26 +1,3 @@
 # -*- coding: utf-8 -*-
 
-Entities_list = [
-    'title',
-    'inv_id',
-    'cust_name',
-    'cust_addr',
-    'cust_zip',
-    'vend_name',
-    'vend_addr',
-    'vend_tel',
-    'vend_fax',
-    'vend_mail',
-    'vend_zip',
-    'inv_date',
-    'acc_bank',
-    'acc_branch',
-    'acc_type',
-    'acc_num',
-    'acc_name',
-    'due_date',
-    'total',
-    'subtotal',
-    'tax',
-    'billing',
-]
+Entities_list = ['subtotal_key', 'item_unitprice_key', 'total', 'item_unitprice', 'acc_num', 'inv_date', 'item_unit_key', 'due_date', 'item_count', 'item_cat_key', 'title', 'item_price', 'subtotal', 'tax_key', 'inv_id', 'item_name_key', 'cust_addr', 'item_price_key', 'item_count_key', 'vend_addr', 'cust_zip', 'vend_tel', 'item_unit', 'tax', 'vend_name', 'vend_fax', 'vend_zip', 'item_cat', 'acc_type', 'acc_name', 'acc_branch', 'billing', 'acc_bank', 'cust_name', 'total_key', 'billing_key', 'item_name']
