e34abeca6f47bd76b84c13e692b6a58bfeda9bb3
Kaoru TANAKA <kaoru.tanaka@acm.org>
2022-08-22 16:31:43 +0900

    delete pretrained flag
    because not working.
    https://github.com/wenwenyu/PICK-pytorch/issues/4

--- model/encoder.py
+++ model/encoder.py
@@ -27,8 +27,7 @@ class Encoder(nn.Module):
                  max_len: int = 100,
                  image_encoder: str = 'resnet50',
                  roi_pooling_mode: str = 'roi_align',
-                 roi_pooling_size: Tuple[int, int] = (7, 7),
-                 pretrained: bool = False):
+                 roi_pooling_size: Tuple[int, int] = (7, 7)):
         '''
         convert image segments and text segments to node embedding.
         :param char_embedding_dim:
@@ -59,15 +58,15 @@ class Encoder(nn.Module):
         self.transformer_encoder = nn.TransformerEncoder(transformer_encoder_layer, num_layers=nlayers)
 
         if image_encoder == 'resnet18':
-            self.cnn = resnet.resnet18(pretrained=pretrained, output_channels=image_feature_dim)
+            self.cnn = resnet.resnet18(output_channels=image_feature_dim)
         elif image_encoder == 'resnet34':
-            self.cnn = resnet.resnet34(pretrained=pretrained, output_channels=image_feature_dim)
+            self.cnn = resnet.resnet34(output_channels=image_feature_dim)
         elif image_encoder == 'resnet50':
-            self.cnn = resnet.resnet50(pretrained=pretrained, output_channels=image_feature_dim)
+            self.cnn = resnet.resnet50(output_channels=image_feature_dim)
         elif image_encoder == 'resnet101':
-            self.cnn = resnet.resnet101(pretrained=pretrained, output_channels=image_feature_dim)
+            self.cnn = resnet.resnet101(output_channels=image_feature_dim)
         elif image_encoder == 'resnet152':
-            self.cnn = resnet.resnet152(pretrained=pretrained, output_channels=image_feature_dim)
+            self.cnn = resnet.resnet152(output_channels=image_feature_dim)
         else:
             raise NotImplementedError()
 
